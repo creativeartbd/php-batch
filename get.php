a5db0b8a232c7cb4dc845db5db1f4eb479f1e47b
creativeartbd <creativeartbd@gmail.com>
2022-02-26 12:14:51 +0600

    PHP Get Method

--- get.php
+++ get.php
@@ -6,7 +6,29 @@
 	<title>GET Method</title>
 </head>
 <body>
-<h1>GET Method</h1>
+
+<h1>All Student List</h1>
+
+<table border="1" cellpadding="10">
+	<tr>
+		<th>Sl.</th>
+		<th>Student Name</th>
+		<th>Student Role</th>
+		<th>Action</th>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>Alex Mojum</td>
+		<td>111</td>		
+		<td><a href="edit.php?roll=111&name=Alex Mojum">Edit</a> | <a href="">Delete</a></td>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>Karim</td>
+		<td>112</td>		
+		<td><a href="edit.php?roll=112&name=Karim">Edit</a> | <a href="">Delete</a></td>
+	</tr>
+</table>
 
 </body>
 </html>
