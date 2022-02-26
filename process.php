befedd58b0cf64b9ccd71779220ad68be902b9ff
creativeartbd <creativeartbd@gmail.com>
2022-02-26 10:47:27 +0600

    PHP FILES

--- process.php
+++ process.php
@@ -10,13 +10,16 @@ $email 			= $_POST['email'];
 $password 	= $_POST['password'];
 $gender 		= $_POST['gender'];
 $country 		= $_POST['country'];
+$file_name	= $_FILES['picture']['name'];
 
 // echo "My name is $fullname and My email address is $email and my password is $password and my gender is $gender and my country name $country";
 
 // echo "My name is $fullname " . " My email addres is $email " . " My password is $password"; 
 
-echo '<pre>';
-	print_r( $_FILES );
-echo '</pre>';
+// echo '<pre>';
+// 	print_r( $_FILES );
+// echo '</pre>';
+
+// echo $_FILES['picture']['type'];
 
 ?>
