7160e4a4ace4533669db4241d2674f1c7a09a5b3
creativeartbd <creativeartbd@gmail.com>
2022-02-26 10:12:43 +0600

    POST Method

--- post.php
+++ post.php
@@ -11,40 +11,40 @@
   <table border="1" cellpadding="10">
     <tr>
       <td>Your Full Name</td>
-      <td><input type="text" /></td>
+      <td><input type="text" name="fullname" /></td>
     </tr>
     <tr>
       <td>Your Email Address</td>
-      <td><input type="email" /></td>
+      <td><input type="email" name="email" /></td>
     </tr>
     <tr>
       <td>Your Password</td>
-      <td><input type="password" /></td>
+      <td><input type="password" name="password" /></td>
     </tr>
     <tr>
       <td>Your Gender</td>
       <td>
-        <input type="radio" name="gender" />Male
-        <input type="radio" name="gender" />Female
+        <input type="radio" name="gender" value="male" />Male
+        <input type="radio" name="gender" value="female" />Female
       </td>
     </tr>
     <tr>
       <td>Your Country</td>
       <td>
-        <select>
-          <option value="">Bangladesh</option>
-          <option value="">America</option>
-          <option value="">Canada</option>
+        <select name="country">
+          <option value="bangladesh">Bangladesh</option>
+          <option value="american">America</option>
+          <option value="canada">Canada</option>
         </select>
       </td>
     </tr>
     <tr>
       <td>Your Profile Picture</td>
-      <td><input type="file"/></td>
+      <td><input type="file" name="picture" /></td>
     </tr>
     <tr>      
       <td align="right" colspan="2">
-        <input type="submit" value="registration"/>
+        <input type="submit" name="submit" value="registration"/>
       </td>
     </tr>
   </table>
