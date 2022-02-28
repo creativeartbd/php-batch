832c87397f02ba100b64fad01239c127be1e4bd3
creativeartbd <creativeartbd@gmail.com>
2022-02-28 10:36:24 +0600

    sidebar

--- project/index.php
+++ project/index.php
@@ -31,19 +31,7 @@
 		<!-- Article end here -->
 		<!-- Sidebar start here -->
 		<div class="col-md-4">
-			<form action="">
-				<table class="table">
-					<tr>
-						<td><input type="text" name="username" placeholder="Enter your username" class="form-control"></td>
-					</tr>
-					<tr>
-						<td><input type="password" name="password" placeholder="Enter your password" class="form-control"></td>
-					</tr>
-					<tr>
-						<td><input type="submit" class="btn btn-primary"></td>
-					</tr>
-				</table>
-			</form>
+			<?php require_once('sidebar.php'); ?>
 		</div>
 		<!-- Sidebar end here -->
 	</div>
---
+++ project/sidebar.php
@@ -0,0 +1,13 @@
+<form action="">
+	<table class="table">
+		<tr>
+			<td><input type="text" name="username" placeholder="Enter your username" class="form-control"></td>
+		</tr>
+		<tr>
+			<td><input type="password" name="password" placeholder="Enter your password" class="form-control"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" class="btn btn-primary"></td>
+		</tr>
+	</table>
+</form>
