e1a804c74324c0ca56eca78a96557e68ac3cb714
creativeartbd <creativeartbd@gmail.com>
2022-02-28 15:35:13 +0600

    Login System

--- project/navbar.php
+++ project/navbar.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
   <div class="container">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,6 +27,15 @@
         <li class="nav-item">
           <a class="nav-link"  href="contact.php">Contact</a>
         </li>
+        <?php 
+        if( isset( $_SESSION['email'] ) ) {
+          ?>
+          <li class="nav-item">
+            <a class="nav-link"  href="logout.php">Logout</a>
+          </li>
+          <?php
+        }
+        ?>
       </ul>      
     </div>
   </div>
--- project/sidebar.php
+++ project/sidebar.php
@@ -1,13 +1,51 @@
-<form action="">
-	<table class="table">
-		<tr>
-			<td><input type="text" name="username" placeholder="Enter your username" class="form-control"></td>
-		</tr>
-		<tr>
-			<td><input type="password" name="password" placeholder="Enter your password" class="form-control"></td>
-		</tr>
-		<tr>
-			<td><input type="submit" class="btn btn-primary"></td>
-		</tr>
-	</table>
-</form>
+<?php 
+if( isset( $_POST['submit'] ) && $_POST['submit'] == 'Login' ) {
+	
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$hash			=	hash('sha512', $password);
+
+	require_once( 'connection.php' );
+
+	$check_sql	=	"SELECT email, password FROM students WHERE email = '$email' AND password = '$hash' ";
+	$check_query = mysqli_query( $connect, $check_sql );
+	$found 	= mysqli_num_rows( $check_query );
+
+	if( empty( $email ) && empty( $password) ) {
+		echo '<div class="alert alert-danger">All fields are required</div>';
+	} else {
+		if( empty( $email) ) {
+			echo '<div class="alert alert-danger">Please enter your email addresss</div>';
+		} elseif( empty( $password) ) {
+			echo '<div class="alert alert-danger">Please enter your password</div>';
+		} elseif( $found !== 1 ) {
+			echo '<div class="alert alert-danger">Either username or password is incorrect</div>';
+		}
+	}
+
+	if( $found == 1 ) {
+		echo '<div class="alert alert-success">Successfully Logged</div>';		
+		$_SESSION['email'] = $email;
+		header("refresh:4;url:admin/index.php");
+		exit();
+	}	
+}
+?>
+
+<?php if( ! isset( $_SESSION['email'] ) ) : ?>
+
+	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+		<table class="table table-bordered">
+			<tr>
+				<td><input type="text" name="email" placeholder="Enter your email" class="form-control" value="<?php if( isset( $_POST['email'] ) ) echo $_POST['email']; ?>"></td>
+			</tr>
+			<tr>
+				<td><input type="password" name="password" placeholder="Enter your password" class="form-control"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="submit" class="btn btn-primary" value="Login"></td>
+			</tr>
+		</table>
+	</form>
+
+<?php endif; ?>
